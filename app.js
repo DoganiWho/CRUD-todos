e06d7f7fd1037ac6fe74c0b0e86aeb57babcf9e0
DoganiWho <96683644+DoganiWho@users.noreply.github.com>
2023-04-18 09:47:28 +0000

    setup mongodb client

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 import { readFile } from 'fs/promises'
 import express from 'express';
+import bodyParser from 'body-parser';
 const app = express();
-
-
+app.use(bodyParser.json())
+// import path from 'path';
 
 // app.listen(PORT, () => {console.log(`Server listening on port ${PORT}`)});
+/*
 
+app.get('/', (req, res) => { 
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/getTodos', (req, res) => {
+
+});
+
+*/
 app.get('/', async (req, res) => { 
     res.send( await readFile('./index.html', 'utf8'))
 });
--- server.js
+++ server.js
@@ -1,31 +1,80 @@
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 import app from './app.js';
-
-
+import { MongoClient, ObjectId } from 'mongodb';
 const {MONGODB_URL} = process.env;
 
-( async function(){
-    mongoose.connect(MONGODB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then( () => {
-        console.log(`Database Connected SUCCESSFULLY`)
+const mongoOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+// const state = {
+//     db: null
+// };
+
+const client = new MongoClient(MONGODB_URL, mongoOptions)
+
+/*
+const connect = (callback) => {
+    if (state.db) {
+        callback();
+    } else {
+        mongoose.connect(MONGODB_URL, mongooseOptions, (err, client) => {
+            if (err) {
+                callback(err);
+            } else {
+                state.db = client.db(dbName);
+                callback();
+            }
+        });
+    }
+}
+
+*/
+async function run(){
+    await client.connect()
+    .then( () => { 
+        client.db("admin").command({ ping: 1 })
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
         const onListen = () => {
             console.log(`Listening on http://localhost:8080` );
         }
-        app.listen(process.env.PORT || 8080, onListen)
+        app.listen(process.env.PORT || 8080, onListen);
     })
     .catch( error => {
-        console.log(`DATABASE CONNECTION FAILED`);
+        console.log('Connection FAILED!');
         console.log(error);
         process.exit(1);
     })
-})()
+  }
+
+run().catch(console.dir);
 
 
+// mongoose.connect(MONGODB_URL, mongooseOptions)
+    // .then( () => {
+    //     console.log(`Database Connected SUCCESSFULLY`);
+    //     const onListen = () => {
+    //         console.log(`Listening on http://localhost:8080` );
+    //     }
+    //     app.listen(process.env.PORT || 8080, onListen);
+    // })
+    // .catch( error => {
+    //     console.log(`DATABASE CONNECTION FAILED`);
+    //     console.log(error);
+    //     process.exit(1);
+    // })
 
 
+// const getPrimaryKey = (_id) => {
+//     return ObjectId;
+// }
+
+// const getDB = () => {
+//     return state.db
+// }
+
+// export {getDB, getPrimaryKey};
 
 /*
 
