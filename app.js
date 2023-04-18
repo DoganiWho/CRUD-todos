7179f2c324de3ddccf1d17daf1f494f0d79a7c94
DoganiWho <96683644+DoganiWho@users.noreply.github.com>
2023-04-18 07:33:11 +0000

    setup root endpoint

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
+import { readFile } from 'fs/promises'
 import express from 'express';
 const app = express();
-const PORT = 8080;
+
 
 
 // app.listen(PORT, () => {console.log(`Server listening on port ${PORT}`)});
 
-app.get('/', (req, res) => { 
-    // res.sendFile(__dirname + './index.html') 
-    res.send("<h1>Server Running!</h1>")
+app.get('/', async (req, res) => { 
+    res.send( await readFile('./index.html', 'utf8'))
 });
 
-app.post('/', (req, res) => {});
+// app.post('/', (req, res) => {});
 
-app.put('/', (req, res) => {});
+// app.put('/', (req, res) => {});
 
-app.delete('/', (req, res) => {});
+// app.delete('/', (req, res) => {});
 
 export default app;
 
--- server.js
+++ server.js
@@ -1,41 +1,66 @@
-
 import mongoose from 'mongoose';
 import app from './app.js';
 
 
-let PORT = 8080
-const MONGODB_URL = process.env;
-/* 
+const {MONGODB_URL} = process.env;
 
-
-exports.connect = () => {
+( async function(){
     mongoose.connect(MONGODB_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
     .then( () => {
-
+        console.log(`Database Connected SUCCESSFULLY`)
+        const onListen = () => {
+            console.log(`Listening on http://localhost:8080` );
+        }
+        app.listen(process.env.PORT || 8080, onListen)
     })
     .catch( error => {
         console.log(`DATABASE CONNECTION FAILED`);
         console.log(error);
         process.exit(1);
     })
-}
+})()
+
+
+
+
+
+/*
+
+( async function(){
+    mongoose.connect(MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then( 
+        console.log(`Database Connected SUCCESSFULLY`)
+        )
+        .catch( error => {
+            console.log(`DATABASE CONNECTION FAILED`);
+            console.log(error);
+        process.exit(1);
+    })
+})()
 
 */
+
+/* 
 
 ( async function(){
     try {
         await mongoose.connect('mongodb://localhost:27017/todos')
         console.log("Connected to Database!");
-
+        
         const onListen = () => {
             console.log(`Listening on port: ${PORT}` );
         }
-
+        
         app.listen(PORT, onListen)
     } catch (error) {
         console.error('Error: ', error);
     }
 })()
+
+*/
