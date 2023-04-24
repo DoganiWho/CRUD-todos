37de9f404ff91c3688c97cb1ffee9b892711ded1
DoganiWho <96683644+DoganiWho@users.noreply.github.com>
2023-04-24 05:52:31 +0000

    reinitialised setup

--- server.js
+++ server.js
@@ -1,106 +1,30 @@
-// import all the necessary packages
-import express from 'express';
-// import cors from 'cors';
-import mongoose from "mongoose";
-import { todosRoutes }from './routes/app.routes.js';
-import { Todo } from './models/app.models.js'; //import TodoSchema
+// if (process.env.NODE_ENV !== 'production') {
+//     require('dotenv').parse()
+// }
 
+const express = require('express');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
+const indexRouter = require('./routes/index');
 
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layout/layout');
+app.use(expressLayouts);
 app.use(express.static('public'));
-// app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// include the todoRoutes
-app.use("/api", todosRoutes);
-
-// DB connection
-const server = 'localhost:27017'
-const database = 'todosAndNotes'
-const PORT = process.env.PORT || 8080;
-
-class Database {
-  constructor() {
-    this._connect();
-  }
-
-  _connect() {
-    mongoose
-      .connect(`mongodb://${server}/${database}`, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .then(() => {
-        console.log('Database connection successful');
-        app.listen(PORT, () => {
-          console.log(`app listening to http://localhost:${PORT}`);
-        });
-      })
-      .catch(err => {
-        console.error('Database connection error', err);
-      });
-  }
-}
-
-
-//get all todos
-app.get('/', (req, res) => {
-  const code = `Welcome to my CRUD todo-app!`;
-  Todo.find()
-  .sort("-createdAt")
-  .then((result) => {
-    res.render("index.ejs", {code, todos: result});
-  }).catch(err => {
-    res.status(400).json({
-    error:
-      err || "Some error occurred while retrieving todos.",
-    });
-  });
-});
-
-app.get('/todos/create', (req, res) => {
-  res.render('new.ejs')
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true, 
+    // useUnifiedTopology: true
 })
+const db = mongoose.connection
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to Mongoose'))
 
-// create a new todo
-app.post('/todos/create', (req, res) => {
-const todo = new Todo(req.body)
+app.use('/', indexRouter);
 
-todo.save().then(
-  doc => {
-    console.log(doc);
-    res.redirect('/todos/create')
-  }).catch(err => {
-  console.error(err);
-  res.render('new.ejs', {value})
-  });
-});
-
-
-/* TODO: 
-app.get("/:id/duplicate", async (req, res) => {
-  const id = req.params.id
-  try {
-    const document = await Document.findById(id)
-    res.render("new", { value: document.value })
-  } catch (e) {
-    res.redirect(`/${id}`)
-  }
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app listening on http://localhost:${process.env.PORT || 3000}`);
 })
-
-app.get("/:id", async (req, res) => {
-  const id = req.params.id
-  try {
-    const document = await Document.findById(id)
-
-    res.render("code-display", { code: document.value, id })
-  } catch (e) {
-    res.redirect("/")
-  }
-})
-*/
-
-
-export default new Database();
